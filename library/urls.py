2749723a320643ce3f9dd79830940683dfbd028f
Diego-82 <naeimiarian82@gmail.com>
2024-08-15 13:37:10 +0330

    masage

--- library/urls.py
+++ library/urls.py
@@ -1,1 +1,9 @@
-from .
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.book_list, name='books'),
+    path('books/<int:pk>', views.book_detail, name='book_detail'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>', views.edit_book, name='edit_book'),
+]
--- library/views.py
+++ library/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from library.models import Book
+from library.forms import BookForm
 
-# Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+
+def book_detail(request, pk):
+    book = Book.objects.get(pk=pk)
+    count = book.author.author_book.count()
+    return render(request, 'book_detail.html', {'book': book,
+                                                'number_of_book': count})
+
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('books')
+    return render(request, 'add_book.html', {'form': BookForm()})
+
+
+def edit_book(request, pk):
+    obj = get_object_or_404(Book, id=pk)
+
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=obj)
+        if form.is_valid():
+            form.save()
+            return redirect('books')
+    return render(request, 'add_book.html', {'form': BookForm(instance=obj)})
+
